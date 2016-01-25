e6e87492d2c269a0ebed37becdafbef6e296cbb6
Alexander <alykoshin@gmail.com>
2016-01-25 22:42:31 +0300

    fixed update-deps

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ var path = require('path');
 var object_assign = require('./lib/assign');
 
 ////////////////////////////////////////////////////////////////////////////////
-// Trick taken from https://github.com/aseemk/requireDir/blob/master/index.js
-//
-// make a note of the calling file's path, so that we can resolve relative
-// paths. this only works if a fresh version of this module is run on every
-// require(), so important: we clear the require() cache each time!
-//
+// Trick taken from https://github.com/aseemk/requireDir/blob/master/index.js //
+//                                                                            //
+// make a note of the calling file's path, so that we can resolve relative    //
+// paths. this only works if a fresh version of this module is run on every   //
+// require(), so important: we clear the require() cache each time!           //
+//                                                                            //
 delete require.cache[__filename];
-//
+//                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
 var parentModule = module.parent;
